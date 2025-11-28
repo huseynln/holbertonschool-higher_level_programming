bfd979df0a3ad81fb20c1b78d37a547d1226be92
huseynln <huseynhastaken@gmail.com>
2025-11-28 10:43:43 +0000

    d

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -77,7 +77,7 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation of the rectangle using #
+        """Return a string representation of the rectangle using print_symbol
 
         If width or height is 0, return an empty string
         """
@@ -85,8 +85,9 @@ class Rectangle:
             return ""
 
         rectangle_str = []
+        symbol = str(self.print_symbol)
         for i in range(self.__height):
-            rectangle_str.append("#" * self.__width)
+            rectangle_str.append(symbol * self.__width)
 
         return "\n".join(rectangle_str)
 
@@ -101,3 +102,39 @@ class Rectangle:
         """Print a message when a Rectangle instance is deleted"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the biggest rectangle based on the area
+
+        Args:
+            rect_1: first Rectangle instance
+            rect_2: second Rectangle instance
+
+        Returns:
+            The rectangle with the bigger area, or rect_1 if equal
+
+        Raises:
+            TypeError: if rect_1 or rect_2 is not a Rectangle instance
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """Return a new Rectangle instance with width == height == size
+
+        Args:
+            cls: the class (Rectangle)
+            size: the size of the square (default: 0)
+
+        Returns:
+            A new Rectangle instance representing a square
+        """
+        return cls(size, size)
